268ae0c0938e51fc5785f6b8ad7a8697b91dea35
Bawan Majid <bawanmajid23@gmail.com>
2020-06-07 13:22:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 
 
+<<<<<<< HEAD
 let append =["Milo", "Otis", "Garfield"];
 
 let prepend =["Milo", "Otis", "Garfield"];
@@ -12,3 +13,10 @@ append.push("Odie");
 prepend.unshift("Odie");
 removeLast.pop();
 removeFirst.shift();
+=======
+let append[] =["Milo", "Otis", "Garfield"];
+
+let prepend[] =["Milo", "Otis", "Garfield"];
+let removeLast[] =["Milo", "Otis", "Garfield"];
+let removeFirst[] =["Milo", "Otis", "Garfield"];
+>>>>>>> 40910d19854e35403facb676df66506656022916
